da1505f4d176c292c1b879809605ce0e856a6ef1
malg1 <malg1@PC82>
2024-04-23 21:05:38 -0300

    Agregar archivos de proyecto.

---
+++ practicaUnoPostaa/Models/Bebida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaUno.Models
+{
+    internal class Bebida
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+
+        public Bebida(string nombre, int cantidad)
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        }
+
+        public void tomarse(int cuantoBebio)
+        {
+            this.cantidad -= cuantoBebio;
+        }
+    }
+}
---
+++ practicaUnoPostaa/Program.cs
@@ -0,0 +1,16 @@
+﻿using practicaUno.Models;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bebida bebida = new Bebida("Coca-Cola", 500);
+        Bebida bebida2 = new Bebida("Fanta", 400);
+
+        bebida.tomarse(100);
+        
+        System.Console.WriteLine("Le quedan " + bebida.cantidad + "a la bebida" + bebida.nombre);
+        System.Console.WriteLine("Le queda" + bebida2.nombre + "a la bebida" + bebida2.nombre);
+
+    }
+}
