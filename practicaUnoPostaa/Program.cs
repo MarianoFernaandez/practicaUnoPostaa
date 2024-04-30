97a040666cc18d4972b2790de7ae9cd3a9594cf9
mariano.fernandezblanco@itisjt.edu.ar <mariano.fernandezblanco@itisjt.edu.ar>
2024-04-29 21:41:31 -0300

    herencia

--- practicaUnoPostaa/Program.cs
+++ practicaUnoPostaa/Program.cs
@@ -1,12 +1,28 @@
 ﻿using practicaUno.Models;
+using practicaUnoPostaa;
 using System;
 class Program
 {
     static void Main(string[] args)
     {
+        int[] numeros = new int[5] {1,2,3,4,5};
+        int numeroguardado = numeros[2];
+        System.Console.WriteLine(numeroguardado);
+        for(int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine("iteracion: " + (i+1) + "-" + numeros[i]);
+        }
+        
+
+        foreach(var numero in numeros) // otra forma de hacer un for
+        {
+            Console.WriteLine(numeros);
+        }
+
+        System.Console.Write(numeros.Length); //largo del vector
         Bebida bebida = new Bebida();
         Bebida bebida2 = new Bebida("Fanta", 400);
-
+        Vino vinoUno = new Vino(50);
         bebida.SetNombre("Coca-Cola");
         bebida.SetCantidad(50);
         bebida.tomarse(100);
@@ -14,8 +30,9 @@ class Program
         bebida2.SetCantidad(100);
         bebida2.tomarse(50);
         
-        System.Console.WriteLine("Le quedan " + bebida.cantidad + "a la bebida" + bebida.nombre);
+        System.Console.WriteLine("\n Le quedan " + bebida.cantidad + "a la bebida" + bebida.nombre);
         System.Console.WriteLine("Le queda" + bebida2.nombre + "a la bebida" + bebida2.nombre);
 
+        System.Console.ReadKey();
     }
 }
---
+++ practicaUnoPostaa/Vino.cs
@@ -0,0 +1,19 @@
+﻿using practicaUno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaUnoPostaa
+{
+    internal class Vino : Bebida
+    {
+        public Vino(int Cantidad, string Nombre = "Vino")
+            : base(Nombre, Cantidad)
+        {
+
+        }
+
+    }
+}
